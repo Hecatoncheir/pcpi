e218b2c80437a811ed629bad3d1ad6ea1d0568fc
Ras <rasart.pro@gmail.com>
2012-02-17 07:23:09 +0000

    "Завершающий этап верстки"

--- js/script.js
+++ js/script.js
@@ -16,20 +16,15 @@ $(document).ready(function() {
 
 
 
+/* Переключатели */
 
+(function($) {
+$(function() {
 
+  $('ul.tabs').delegate('li:not(.current)', 'click', function() {
+    $(this).addClass('current').siblings().removeClass('current')
+      .parents('body').find('div.box').hide().eq($(this).index()).fadeIn(150);
+  })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+})
+})(jQuery)
